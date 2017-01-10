7541f60ad0ae012bd2a6a005a9255847b866accc
super zozo <Anh.Nguyen3@metropolia.fi>
2017-01-10 19:01:25 +0700

    add navbar

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Form from "./Form.jsx";
+import { Navbar } from "react-bootstrap";
 import Confirmation from "./Confirmation.jsx";
 import TableWrapper from "./TableWrapper.jsx";
 
 const App = () => {
-    return <div className="container">
-	    		<Form></Form>
-	    		<Confirmation/>
-	    		<TableWrapper/>
+    return <div>
+    			<nav className="navbar navbar-default">
+				        <a href="#"><img src="static/logo.png" className="img-responsive" alt=""/></a>
+				</nav>
+    			<div className="container">
+		    		<Form></Form>
+		    		<Confirmation/>
+		    		<TableWrapper/>
+		    	</div>
 	    	</div>
 }
 
